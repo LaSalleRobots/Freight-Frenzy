ec4c1bb22cc59f3fe09e4a74d7ce39ffc882b930
Lukas Werner <lwerner@lshigh.org>
2021-12-22 23:31:43 -0800

    Make red full autonomous code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AIRedAuto.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Full AI Auto", group = "AI")
+public class AIRedAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ElapsedTime runtime = new ElapsedTime();
+        RoboHelper robot = new RoboHelper(hardwareMap, runtime);
+
+        TeamElementTracker teamElementTracker = new TeamElementTracker();
+        OpenCVPipelineRunner vision = new OpenCVPipelineRunner(hardwareMap, teamElementTracker);
+        vision.start(); // lol we can peep here.
+
+        waitForStart();
+        TeamElementTracker.Position position = teamElementTracker.getPosition();
+        robot.drive.right().goFor(1); // get to the alliance shipping hub
+        // during our approach lets make the arm go the the correct level
+        switch (position) {
+            case Left:
+                robot.arm.setPositionAsync(20); // bottom level
+            case Center:
+                robot.arm.setPositionAsync(60); // middle level
+            case Right:
+                robot.arm.setPositionAsync(90); // top level
+        }
+        // head to the shipping hub
+        robot.drive.forward().goFor(1);
+        // now that we are there lets drop the element
+        robot.arm.gripper.open();
+        // viola +(20pts) correct level AI bonus
+
+        robot.drive.backward().goFor(.75); // align ourselves for the carousel
+        robot.drive.left().goFor(3); // head over to the carousel
+        robot.startSpinner().runFor(1); // do a duck!
+        // +(10pts) delivered duck
+        robot.drive.right().goFor(1); // prep for a wall slide
+        robot.drive.backward().goFor(.25); // wall SLAM!
+        robot.drive.right().goFor(6); // park in the warehouse
+        // +(10pts) parking
+
+    }
+}
